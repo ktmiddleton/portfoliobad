58093eb0196addd1a171f2d92abef6d9472e5222
Kobe Middleton <kobe.middleton62@gmail.com>
2022-12-18 20:47:03 -0500

    git commit test

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar"> </nav>
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    Portfolio 
+                </Link>
+            </div>
+    </>
+  )
+}
+
+export default Navbar
