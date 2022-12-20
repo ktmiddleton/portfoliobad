619fd158b1629cbe882b75a8690512a4ab6dce4a
Kobe Middleton <kobe.middleton62@gmail.com>
2022-12-20 03:28:33 -0500

    some progress, got navbar working (mostly) although it is unpretty

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
         <nav className="navbar"> </nav>
@@ -9,6 +14,27 @@ function Navbar() {
                 <Link to="/" className="navbar-logo">
                     Portfolio 
                 </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                  <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                  <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                      Home
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                      Projects
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/about-me' className='nav-links' onClick={closeMobileMenu}>
+                      About Me
+                    </Link>
+                  </li>
+                </ul>
+                {button ?}
             </div>
     </>
   )
